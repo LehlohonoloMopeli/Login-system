6bb7dc83f559ceb6f5a872ad06ab7327b4c6b545
LehlohonoloMopeli <lehlohonolompl@gmail.com>
2021-04-13 15:55:12 +0200

    Changed command for installing packages

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 """
     To create virtual environment $: python -m venv env
     To activate virtual environment $: source ".\env\Scripts\activate"
-    To install the requirements we run $: pip  install -r app\requirements.txt
+    To install the requirements we run $: pip install -r requirements.txt
     To update requirements.txt after new pip install use $: pip freeze > requirements.txt
     To run  the program use the following command $: hypercorn app.main:app --reload
     To deactivate virtual enviroment $: deactivate
